171939a22efbe2d7f5cf46e252ad3987e95de001
Peter Fulop <fulop.peter@datalytix.tech>
2024-09-11 11:49:53 +0000

    query created to run on Polars dataframes

--- src/sql_context/polars/running_sql_queries.py
+++ src/sql_context/polars/running_sql_queries.py
@@ -20,3 +20,22 @@ movie_genre_parquet_file_path = os.path.abspath(
     os.path.join(data_provider_path, "../data", "movie_genre.parquet")
 )
 
+df_movie_genre = pl.read_parquet(movie_genre_parquet_file_path)
+
+sql_context = pl.SQLContext()
+sql_context.register("movies", df_movies)
+sql_context.register("movie_genre", df_movie_genre)
+
+df_movies_with_no_genre = sql_context.execute(
+    """
+    SELECT 
+        m.movie_id, m.title, m.release_date
+    FROM
+        movies m
+    LEFT JOIN movie_genre mg ON m.movie_id = mg.movie_id
+    WHERE
+        mg.genre_id IS NULL
+    ORDER BY
+        m.release_date    
+    """).collect()
+print(df_movies_with_no_genre)
